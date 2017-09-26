55f0cec2d73bdb7c66e8821e46f081c6be2d74b4
kaistullich <kairstullich@gmail.com>
2017-09-25 23:41:59 -0700

    removed draw_rect() method, changed constructor

--- Pong.py
+++ Pong.py
@@ -1,35 +1,22 @@
-class Rectangles:
+class Rectangle:
     """
     Class for creating the Paddles inside of the Pong game.
     """
 
-    def __init__(self, screen, color, rect, width=0):
+    def __init__(self, screen, coords=None, width=0):
         """
         Initializes a pygame rectangle
 
         :param screen: the surface on which to draw the rectangle
 
-        :param color:  the color the rectangle should be. Needs to be
-                       RGB color. ( i.e. BLUE = (0, 0, 255) )
-
-        :param rect:   the location where the rectangle should be drawn,
-                       this is a `list` with coordinates [x1, y1, width, height]
+        :param coords: the location where the rectangle should be drawn,
+                            this is a `list` with coordinates [x1, y1, width, height]
 
         :param width:  the width of the border around the rectangle, the
                        the default will be set to 0 so the entire rectangle
                        will be filled in with the color given.
         """
         self.screen = screen
-        self.color = color
-        self.rect = rect
+        self.color = (255, 255, 255)
+        self.coords = coords
         self.width = width
-        self.game = None
-
-    def draw_rect(self, game):
-        """
-        Draws the actual pygame rectangle
-
-        :param game: the pygame library
-        """
-        self.game = game
-        self.game.draw.rect(self.screen, self.color, self.rect, self.width)
