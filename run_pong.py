5addd0a9ac2e0ebc2e9b0162325c18f4d1e76232
kaistullich <kairstullich@gmail.com>
2017-09-26 11:26:20 -0700

    Key pressed events
    Created key pressed events for keys "Q, A, P, L"

--- run_pong.py
+++ run_pong.py
@@ -19,14 +19,14 @@ swidth = info.current_w
 sheight = info.current_h
 
 
-def draw_environment(dividers):
+def draw_environment(dividers, y_loc):
     # black background
     screen.fill(BLACK)
 
     # create and draw the 2 paddles
     # TODO: figure out how to make the paddles move with up arrows
-    paddle1 = Rectangle(screen, coords=[30, 350, 13, 70])
-    paddle2 = Rectangle(screen, coords=[757, 350, 13, 70])
+    paddle1 = Rectangle(screen, coords=[30, y_loc, 13, 70])
+    paddle2 = Rectangle(screen, coords=[757, y_loc, 13, 70])
     pygame.draw.rect(screen, paddle1.color, paddle1.coords)
     pygame.draw.rect(screen, paddle2.color, paddle2.coords)
 
@@ -56,13 +56,32 @@ def main():
     # create 30 rectangles for the dividers
     dividers = [Rectangle(screen) for i in range(NUMBER_OF_DIVIDERS)]
 
+    y_move = int(HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            # all keys
+            keys = pygame.key.get_pressed()
+
+            # quit game
+            if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
                 pygame.quit()
                 quit()
 
-        draw_environment(dividers)
+            # check key presses
+            elif event.type == pygame.KEYDOWN:
+                # TODO: adjust the paddles to go up/down with key presses
+                if keys[pygame.K_a]:
+                    print("Left Paddle 'A' pressed")
+                if keys[pygame.K_q]:
+                    print("Left Paddle 'Q' pressed")
+                if keys[pygame.K_p]:
+                    print("Right paddle 'P' pressed")
+                if keys[pygame.K_l]:
+                    print("Right paddle 'L' pressed")
+
+        draw_environment(dividers, y_move)
+        y_move += 50
         clock.tick(60)
 
 
